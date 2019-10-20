3d102083016afac69bf5ee4d4c75cf709ed06485
Alexander Ramirez Kiriushenko <jaxxbot.cr@gmail.com>
2019-10-20 13:46:07 -0600

    Corrected file names, added one more database.

--- Backup & Restore/restore.sql
+++ Backup & Restore/restore.sql
@@ -2,8 +2,16 @@ USE [master]
 
 RESTORE DATABASE AdventureWorksDW2017
 FROM disk= 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\Backup\AdventureWorksDW2017.bak'
-WITH MOVE 'AdventureWorksDW2017_data' 
+WITH MOVE 'AdventureWorksDW2017' 
 TO 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\AdventureWorksDW2017.mdf',
 MOVE 'AdventureWorksDW2017_Log' 
 TO 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\AdventureWorksDW2017.ldf'
+,REPLACE
+
+RESTORE DATABASE AdventureWorks2017
+FROM disk= 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\Backup\AdventureWorks2017.bak'
+WITH MOVE 'AdventureWorks2017' 
+TO 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\AdventureWorks2017.mdf',
+MOVE 'AdventureWorks2017_Log' 
+TO 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\AdventureWorks2017.ldf'
 ,REPLACE
